89b7ae83bf39ec5a3e231d39370e145077cf45ca
robdav2143 <robdav2143@yahoo.com>
2016-08-14 20:44:21 -0400

    Added Project
    Unity Project Added

---
+++ Assets/EasyMove.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class EasyMove : MonoBehaviour {
+
+    public float Speed = 8;
+    public float acceleration = 12;
+
+    public float currentSpeed;
+    public float targetSpeed;
+
+    private Vector2 amountToMove;
+
+    private PlayerPhysics playerPhysics;
+
+    // Use this for initialization
+    void Start () {
+        playerPhysics = GetComponent<PlayerPhysics>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        targetSpeed = Input.GetAxisRaw("Horizontal") * Speed;
+        currentSpeed = incrementTowards(currentSpeed, targetSpeed, acceleration);
+
+        amountToMove = new Vector2(currentSpeed, 0);
+        playerPhysics.Move(amountToMove * Time.deltaTime);
+    }
+
+    private float incrementTowards(float n, float target, float a)
+    {
+        if (n == target)
+        {
+            return n;
+        }
+
+        else {
+            float dir = Mathf.Sign(target - n);
+            n += a * Time.deltaTime * dir;
+            return (dir == Mathf.Sign(target - n)) ? n : target;
+        }
+    }
+}
